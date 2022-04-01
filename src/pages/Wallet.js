1a2951ba74daeb1278ae40f18f2ae14acef8481f
isagomachado <isa_gomes_machado@hotmail.com>
2022-04-01 17:38:55 -0300

    Requisito 5 - feito

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -8,8 +8,14 @@ class Wallet extends React.Component {
   constructor() {
     super();
 
+    this.handleChange = this.handleChange.bind(this);
+
     this.state = {
       totalExpense: 0,
+      // price: 0,
+      // description: '',
+      // selectCurrencie: '',
+      // pay: '',
     };
   }
 
@@ -23,19 +29,92 @@ class Wallet extends React.Component {
     walletCurrenciesDispatch(requestedCoins);
   }
 
+  handleChange({ target }) {
+    const { name, value } = target;
+
+    this.setState({
+      [name]: value,
+    }, this.changeDisabled);
+  }
+
   render() {
     const { totalExpense } = this.state;
-    const { email } = this.props;
+    const { email, currencies } = this.props;
 
     return (
       <div>
         <header>
           <p data-testid="email-field">{ email }</p>
-          {' '}
-          <p data-testid="total-field">{ totalExpense }</p>
-          {' '}
-          <p data-testid="header-currency-field">BRL</p>
+          <div>
+            <p data-testid="total-field">{ totalExpense }</p>
+            <p data-testid="header-currency-field">BRL</p>
+          </div>
         </header>
+        <form>
+          <label htmlFor="price">
+            Preço:
+            {' '}
+            <input
+              type="text"
+              data-testid="value-input"
+              name="price"
+              id="price"
+            />
+          </label>
+          <br />
+          <label htmlFor="description">
+            Descrição:
+            {' '}
+            <input
+              type="text"
+              data-testid="description-input"
+              name="description"
+              id="description"
+            />
+          </label>
+          <br />
+          <label htmlFor="selectCurrencie">
+            Moeda:
+            {' '}
+            <select
+              name="selectCurrencie"
+              id="selectCurrencie"
+            >
+              {
+                currencies.map((currencie) => (
+                  <option
+                    key={ currencie }
+                    value={ currencie }
+                  >
+                    { currencie }
+                  </option>
+                ))
+              }
+            </select>
+          </label>
+          <br />
+          <label htmlFor="pay">
+            Pagamento:
+            {' '}
+            <select id="pay" data-testid="method-input">
+              <option value="Dinheiro">Dinheiro</option>
+              <option value="Cartão de crédito">Cartão de crédito</option>
+              <option value="Cartão de débito">Cartão de débito</option>
+            </select>
+          </label>
+          <br />
+          <label htmlFor="tag">
+            Categoria:
+            {' '}
+            <select id="tag" data-testid="tag-input">
+              <option value="Alimentação">Alimentação</option>
+              <option value="Lazer">Lazer</option>
+              <option value="Trabalho">Trabalho</option>
+              <option value="Transporte">Transporte</option>
+              <option value="Saúde">Saúde</option>
+            </select>
+          </label>
+        </form>
       </div>
     );
   }
@@ -43,6 +122,7 @@ class Wallet extends React.Component {
 
 const mapStateToProps = (state) => ({
   email: state.user.email,
+  currencies: state.wallet.currencies,
 });
 
 const mapDispatchToProps = (dispatch) => ({
@@ -53,6 +133,9 @@ const mapDispatchToProps = (dispatch) => ({
 Wallet.propTypes = {
   email: PropTypes.string.isRequired,
   walletCurrenciesDispatch: PropTypes.func.isRequired,
+  currencies: PropTypes.shape({
+    map: PropTypes.func,
+  }).isRequired,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Wallet);
